8ddf156f792fe9d73100b58845f9f4abf9ddfc37
mhsjlw <mhsjlw@aol.com>
2015-10-25 07:31:04 -0400

    Update category.rb

--- lib/thepiratebay/category.rb
+++ lib/thepiratebay/category.rb
@@ -1,9 +1,44 @@
 module ThePirateBay
   module Category
-    Audio          = "100"
-    Video          = "200"
-    Applications   = "300"
-    Games          = "400"
-    Other          = "600"
+    All = "100"
+    
+    # Audio
+    Music = "101"
+    Audio_Books = "102"
+    Audio_Clips = "103"
+    FLAC = "104"
+    Other_Audio = "199"
+    
+    # Video
+    Movies = "201"
+    Movies_DVDR = "202"
+    Music_Videos = "203"
+    Movie_Clips = "204"
+    TV_Shows = "205"
+    Video_Handheld = "206"
+    HD_Movies = "207"
+    HD_TV_Shows = "208"
+    Video_3D = "209"
+    Other_Video = "299"
+    
+    # Applications
+    Windows = "301"
+    Mac = "302"
+    UNIX = "303"
+    Application_Handheld = "304"
+    IOS = "305"
+    Android = "306"
+    Other_OS = "399"
+    
+    # Games
+    Games_PC = "401"
+    Games_Mac = "402"
+    Games_PSx = "403"
+    Games_Xbox = "404"
+    Games_Wii = "405"
+    Games_Handheld = "406"
+    Games_IOS = "407"
+    Games_Android = "408"
+    Other_Games = "499"
   end
 end
